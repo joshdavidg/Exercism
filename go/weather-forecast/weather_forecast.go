f5f8a790351f8f3d259e4d06798d5b4f3860e3ae
Josh-Windows <joshdavidg.13@gmail.com>
2022-10-05 22:21:17 -0600

    completed the weather_forecast exercise

--- go/weather-forecast/weather_forecast.go
+++ go/weather-forecast/weather_forecast.go
@@ -1,8 +1,13 @@
+//Package weather provides current weather conditions for a city.
 package weather
 
+//CurrentCondition is used to indicate the current weather condition for the city.
 var CurrentCondition string
+
+//CurrentLocation is used to indicate the current location for the weather forecast.
 var CurrentLocation string
 
+//Forecast takes in a city and condition and returns a forecast of the current weather for the given city.
 func Forecast(city, condition string) string {
 	CurrentLocation, CurrentCondition = city, condition
 	return CurrentLocation + " - current weather condition: " + CurrentCondition
