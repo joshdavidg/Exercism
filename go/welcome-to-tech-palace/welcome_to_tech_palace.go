e452a2b33af2503de5b2fe44bad1cc226cc4eec0
Josh-Windows <joshdavidg.13@gmail.com>
2022-10-08 17:07:00 -0600

    completed welcome to tech palace go exercise

--- go/welcome-to-tech-palace/welcome_to_tech_palace.go
+++ go/welcome-to-tech-palace/welcome_to_tech_palace.go
@@ -1,16 +1,31 @@
 package techpalace
 
+import "strings"
+
 // WelcomeMessage returns a welcome message for the customer.
 func WelcomeMessage(customer string) string {
-	panic("Please implement the WelcomeMessage() function")
+	var builder strings.Builder
+	builder.WriteString("Welcome to the Tech Palace, ")
+	builder.WriteString(strings.ToUpper(customer))
+	return builder.String()
 }
 
 // AddBorder adds a border to a welcome message.
 func AddBorder(welcomeMsg string, numStarsPerLine int) string {
-	panic("Please implement the AddBorder() function")
+	var starBuilder strings.Builder
+	var finalString strings.Builder
+	for i := 0; i < numStarsPerLine; i++ {
+		starBuilder.WriteString("*")
+	}
+	finalString.WriteString(starBuilder.String())
+	finalString.WriteString("\n")
+	finalString.WriteString(welcomeMsg)
+	finalString.WriteString("\n")
+	finalString.WriteString(starBuilder.String())
+	return finalString.String()
 }
 
 // CleanupMessage cleans up an old marketing message.
 func CleanupMessage(oldMsg string) string {
-	panic("Please implement the CleanupMessage() function")
+	return strings.Trim(oldMsg, "*\n ")
 }
