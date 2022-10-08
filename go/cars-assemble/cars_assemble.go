7baad613b830bc09edf82b0caf31799ffd7ac60e
Josh-Windows <joshdavidg.13@gmail.com>
2022-10-08 15:30:24 -0600

    completed the cars assemble exercise.

--- go/cars-assemble/cars_assemble.go
+++ go/cars-assemble/cars_assemble.go
@@ -1,18 +1,27 @@
 package cars
 
+// costPerCar represents the amount of money required to produce one car.
+const costPerCar = 10000
+
+//costPer10Cars is the amount of money require to produce a group of 10 cars.
+const costPer10Cars = 95000
+
 // CalculateWorkingCarsPerHour calculates how many working cars are
 // produced by the assembly line every hour.
 func CalculateWorkingCarsPerHour(productionRate int, successRate float64) float64 {
-	panic("CalculateWorkingCarsPerHour not implemented")
+	percentage := successRate / 100
+	return float64(productionRate) * percentage
 }
 
 // CalculateWorkingCarsPerMinute calculates how many working cars are
 // produced by the assembly line every minute.
 func CalculateWorkingCarsPerMinute(productionRate int, successRate float64) int {
-	panic("CalculateWorkingCarsPerMinute not implemented")
+	return int(CalculateWorkingCarsPerHour(productionRate, successRate)) / 60
 }
 
 // CalculateCost works out the cost of producing the given number of cars.
 func CalculateCost(carsCount int) uint {
-	panic("CalculateCost not implemented")
+	groupsOf10 := carsCount / 10
+	remainingCars := carsCount % 10
+	return uint(groupsOf10)*costPer10Cars + uint(remainingCars)*costPerCar
 }
