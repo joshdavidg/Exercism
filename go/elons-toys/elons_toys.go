c463f9c34cf30cfeba65be30287def59d066bb77
Josh-Windows <joshdavidg.13@gmail.com>
2022-12-31 21:23:14 -0700

    completed elons toys go exercise

--- go/elons-toys/elons_toys.go
+++ go/elons-toys/elons_toys.go
@@ -1,9 +1,29 @@
 package elon
 
-// TODO: define the 'Drive()' method
+import (
+	"fmt"
+)
 
-// TODO: define the 'DisplayDistance() string' method
+//Drive increases distance by the speed of the car and reduces batter power by the batteryDrain
+func (c *Car) Drive() {
+	if c.battery < c.batteryDrain {
+		return
+	}
+	c.battery = c.battery - c.batteryDrain
+	c.distance = c.distance + c.speed
+}
 
-// TODO: define the 'DisplayBattery() string' method
+//DisplayDistance displays the current distance the car has driven
+func (c *Car) DisplayDistance() string {
+	return fmt.Sprintf("Driven %d meters", c.distance)
+}
 
-// TODO: define the 'CanFinish(trackDistance int) bool' method
+//DisplayBattery displays the current battery percentage
+func (c *Car) DisplayBattery() string {
+	return fmt.Sprintf("Battery at %d%%", c.battery)
+}
+
+//CanFinish determines whether a car can finish race given the track distance
+func (c *Car) CanFinish(trackDistance int) bool {
+	return (c.battery/c.batteryDrain)*c.speed >= trackDistance
+}
