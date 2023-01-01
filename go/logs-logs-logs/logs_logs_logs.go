5a57852b3983566930f22e9c0fb49b97883d790c
Josh-Windows <joshdavidg.13@gmail.com>
2022-12-31 22:12:38 -0700

    completed the logs logs logs go exercise

--- go/logs-logs-logs/logs_logs_logs.go
+++ go/logs-logs-logs/logs_logs_logs.go
@@ -1,18 +1,47 @@
 package logs
 
+import (
+	"fmt"
+	"strings"
+	"unicode/utf8"
+)
+
 // Application identifies the application emitting the given log.
 func Application(log string) string {
-	panic("Please implement the Application() function")
+	for _, c := range log {
+
+		if c == '❗' {
+			return "recommendation"
+		}
+
+		if c == '🔍' {
+			return "search"
+		}
+
+		if c == '☀' {
+			return "weather"
+		}
+	}
+
+	return "default"
 }
 
 // Replace replaces all occurrences of old with new, returning the modified log
 // to the caller.
 func Replace(log string, oldRune, newRune rune) string {
-	panic("Please implement the Replace() function")
+	var sBuilder strings.Builder
+	runeSlice := []rune(log)
+	for i, r := range runeSlice {
+		if r == oldRune {
+			runeSlice[i] = newRune
+		}
+		fmt.Fprintf(&sBuilder, "%c", runeSlice[i])
+	}
+	return sBuilder.String()
 }
 
 // WithinLimit determines whether or not the number of characters in log is
 // within the limit.
 func WithinLimit(log string, limit int) bool {
-	panic("Please implement the WithinLimit() function")
+	return utf8.RuneCountInString(log) <= limit
 }
