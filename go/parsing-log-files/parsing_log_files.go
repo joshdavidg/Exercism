339011479279b60ba2bdc080c30edadfd1964c35
Josh-Windows <joshdavidg.13@gmail.com>
2023-01-08 21:38:24 -0700

    completed the parsing log files go exercise

--- go/parsing-log-files/parsing_log_files.go
+++ go/parsing-log-files/parsing_log_files.go
@@ -1,21 +1,50 @@
 package parsinglogfiles
 
+import (
+	"fmt"
+	"regexp"
+	"strings"
+)
+
 func IsValidLine(text string) bool {
-	panic("Please implement the IsValidLine function")
+	re := regexp.MustCompile(`^\[[INF]*[ERR]*[TRC]*[DBG]*[WRN]*[FTL]*\]`)
+	return re.MatchString(text)
 }
 
 func SplitLogLine(text string) []string {
-	panic("Please implement the SplitLogLine function")
+	re := regexp.MustCompile(`(<[~=\-\*]*>)+`)
+	return re.Split(text, -1)
 }
 
 func CountQuotedPasswords(lines []string) int {
-	panic("Please implement the CountQuotedPasswords function")
+	re := regexp.MustCompile(`(?i)".*password.*"`)
+	count := 0
+	for _, line := range lines {
+		if re.MatchString(line) {
+			count = count + 1
+		}
+	}
+	return count
 }
 
 func RemoveEndOfLineText(text string) string {
-	panic("Please implement the RemoveEndOfLineText function")
+	re := regexp.MustCompile(`end-of-line[0-9]+`)
+	return re.ReplaceAllString(text, "")
 }
 
 func TagWithUserName(lines []string) []string {
-	panic("Please implement the TagWithUserName function")
+	re := regexp.MustCompile(`User\s+[[:alnum:]]+\s{1}`)
+	var regexStr string
+	var userName string
+	var found bool
+	for i := range lines {
+		if re.MatchString(lines[i]) {
+			regexStr = re.FindString(lines[i])
+			_, userName, found = strings.Cut(regexStr, " ")
+			if found {
+				lines[i] = fmt.Sprintf("[USR] %s %s", strings.Trim(userName, " "), lines[i])
+			}
+		}
+	}
+	return lines
 }
