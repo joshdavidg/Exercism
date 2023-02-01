bbb3bedc0dc14ae322fc83ee65801f912785430b
Josh-Windows <joshdavidg.13@gmail.com>
2023-01-31 21:53:34 -0700

    completed the easy hamming go exercise

--- go/Easy/hamming/hamming.go
+++ go/Easy/hamming/hamming.go
@@ -1,5 +1,22 @@
+//Package hamming implements functions to calculate the hamming distance between two DNA strands.
 package hamming
 
+import "errors"
+
+// Distance calculates and returns the hamming distance between the two strings a and b.
+// Both strings should be of the same length.
 func Distance(a, b string) (int, error) {
-	panic("Implement the Distance function")
+	if len(a) != len(b) {
+		return 0, errors.New("Lengths of strings must be equal")
+	}
+
+	hammingDist := 0
+
+	for i := 0; i < len(a); i++ {
+		if a[i] != b[i] {
+			hammingDist += 1
+		}
+	}
+
+	return hammingDist, nil
 }
