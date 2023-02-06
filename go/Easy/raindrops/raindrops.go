17de1a033927cd85790e0b81c867c754feced1ba
josh <joshdavidg.13@gmail.com>
2023-02-06 21:55:01 +0000

    completed the easy raindrops go exercise

--- go/Easy/raindrops/raindrops.go
+++ go/Easy/raindrops/raindrops.go
@@ -1,5 +1,30 @@
+// Package raindrops implements functions to return a string of raindrop sounds.
 package raindrops
 
+import (
+	"strconv"
+	"strings"
+)
+
+// Convert returns a string of raindrop sounds based on rules that parse the number passed in.
+// If the number does not fit the rules, the function will simply return the number as a string.
 func Convert(number int) string {
-	panic("Please implement the Convert function")
+	var s strings.Builder
+	if number%3 == 0 {
+		s.WriteString("Pling")
+	}
+
+	if number%5 == 0 {
+		s.WriteString("Plang")
+	}
+
+	if number%7 == 0 {
+		s.WriteString("Plong")
+	}
+
+	if s.Len() == 0 {
+		return strconv.Itoa(number)
+	}
+
+	return s.String()
 }
